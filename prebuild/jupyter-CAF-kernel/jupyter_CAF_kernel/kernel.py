fd5868395e80bc6a806b4a79e168c64d9d65081f
Izaak Beekman <zbeekman@gmail.com>
2017-06-13 17:59:09 -0400

    Nearly working, issues in kernel.py stil persist

---
+++ prebuild/jupyter-CAF-kernel/jupyter_CAF_kernel/kernel.py
@@ -0,0 +1,199 @@
+from __future__ import print_function
+
+from queue import Queue
+from threading import Thread
+
+metakernel = False
+try:
+    from metakernel import MetaKernel as Kernel
+    metakernel = True
+except:
+    try:
+        from ipykernel.kernelbase import Kernel
+    except ImportError:
+        from IPython.kernel.zmq.kernelbase import Kernel
+
+try:
+    from metakernel import register_ipython_magics
+except:
+    from ipykernel.kernelbase import register_ipython_magics
+
+register_ipython_magics()
+
+import re
+import subprocess
+import tempfile
+import os
+import os.path as path
+
+
+class RealTimeSubprocess(subprocess.Popen):
+    """
+    A subprocess that allows to read its stdout and stderr in real time
+    """
+
+    def __init__(self, cmd, write_to_stdout, write_to_stderr):
+        """
+        :param cmd: the command to execute
+        :param write_to_stdout: a callable that will be called with chunks of data from stdout
+        :param write_to_stderr: a callable that will be called with chunks of data from stderr
+        """
+        self._write_to_stdout = write_to_stdout
+        self._write_to_stderr = write_to_stderr
+
+        super(RealTimeSubprocess, self).__init__(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=0)
+
+        self._stdout_queue = Queue()
+        self._stdout_thread = Thread(target=RealTimeSubprocess._enqueue_output, args=(self.stdout, self._stdout_queue))
+        self._stdout_thread.daemon = True
+        self._stdout_thread.start()
+
+        self._stderr_queue = Queue()
+        self._stderr_thread = Thread(target=RealTimeSubprocess._enqueue_output, args=(self.stderr, self._stderr_queue))
+        self._stderr_thread.daemon = True
+        self._stderr_thread.start()
+
+    @staticmethod
+    def _enqueue_output(stream, queue):
+        """
+        Add chunks of data from a stream to a queue until the stream is empty.
+        """
+        for line in iter(lambda: stream.read(4096), b''):
+            queue.put(line)
+        stream.close()
+
+    def write_contents(self):
+        """
+        Write the available content from stdin and stderr where specified when the instance was created
+        :return:
+        """
+
+        def read_all_from_queue(queue):
+            res = b''
+            size = queue.qsize()
+            while size != 0:
+                res += queue.get_nowait()
+                size -= 1
+            return res
+
+        stdout_contents = read_all_from_queue(self._stdout_queue)
+        if stdout_contents:
+            self._write_to_stdout(stdout_contents)
+        stderr_contents = read_all_from_queue(self._stderr_queue)
+        if stderr_contents:
+            self._write_to_stderr(stderr_contents)
+
+
+class CAFKernel(Kernel):
+    implementation = 'jupyter_CAF_kernel'
+    implementation_version = '0.1.0'
+    language = 'Fortran'
+    language_version = 'Fortran 2015'
+    language_info = {'name': 'Fortran',
+                     'mimetype': 'text/x-fortran',
+                     'pygments_lexer': 'fortran',
+                     'codemirror_mode': 'fortran',
+                     'file_extension': '.f90'}
+    banner = "Coarray Fortran kernel.\n" \
+             "Uses GFortran & OpenCoarrays & creates source code files and executables in a temporary folder.\n"
+    magic_prefixes = dict(magic='%', shell='$', help='?')
+
+    def __init__(self, *args, **kwargs):
+        super(CAFKernel, self).__init__(*args, **kwargs)
+        self.files = []
+
+    def cleanup_files(self):
+        """Remove all the temporary files created by the kernel"""
+        for file in self.files:
+            os.remove(file)
+
+    def new_temp_file(self, **kwargs):
+        """Create a new temp file to be deleted when the kernel shuts down"""
+        # We don't want the file to be deleted when closed, but only when the kernel stops
+        kwargs['delete'] = False
+        kwargs['mode'] = 'w'
+        file = tempfile.NamedTemporaryFile(**kwargs)
+        self.files.append(file.name)
+        return file
+
+    def _write_to_stdout(self, contents):
+        self.send_response(self.iopub_socket, 'stream', {'name': 'stdout', 'text': contents})
+
+    def _write_to_stderr(self, contents):
+        self.send_response(self.iopub_socket, 'stream', {'name': 'stderr', 'text': contents})
+
+    def create_jupyter_subprocess(self, cmd):
+        return RealTimeSubprocess(cmd,
+                                  lambda contents: self._write_to_stdout(contents.decode()),
+                                  lambda contents: self._write_to_stderr(contents.decode()))
+
+    def compile_with_caf_wrapper(self, source_filename, binary_filename, fcflags=None, ldflags=None):
+        args = ['caf', source_filename] + fcflags + ['-o', binary_filename] + ldflags
+        return self.create_jupyter_subprocess(args)
+
+    def _filter_magics(self, code):
+
+        magics = {'cflags': [],
+                  'ldflags': [],
+                  'args': [],
+                  'num_procs': 4}
+
+        if "NUM_PROCS" in os.environ:
+            magics['num_procs'] = int(os.environ['NUM_PROCS'])
+
+        for line in code.splitlines():
+            if line.startswith(('%%', '%')):
+                key, value = line[2:].split(":", 2)
+                key = key.strip().lower()
+
+                if key in ['ldflags', 'fcflags']:
+                    for flag in value.split():
+                        magics[key] += [flag]
+                elif key == 'args':
+                    # Split arguments respecting quotes
+                    for argument in re.findall(r'(?:[^\s,"]|"(?:\\.|[^"])*")+', value):
+                        magics['args'] += [argument.strip('"')]
+                elif key == 'num_procs':
+                    magics['num_procs'] = int(value)
+                else:
+                    super(CAFKernel,self).call_magic(line)
+            elif line.startswith(('$', '?')):
+                super(CAFKernel,self).call_magic(line)
+
+        return magics
+
+    def do_execute(self, code, silent, store_history=True,
+                   user_expressions=None, allow_stdin=False):
+
+        magics = self._filter_magics(code)
+
+        with self.new_temp_file(suffix='.f90') as source_file:
+            for line in code:
+                if line.startswith(('%', '%%', '$', '?')):
+                    continue
+                source_file.write(line)
+            source_file.flush()
+            with self.new_temp_file(suffix='.out') as binary_file:
+                p = self.compile_with_caf_wrapper(source_file.name, binary_file.name, magics['fcflags'], magics['ldflags'])
+                while p.poll() is None:
+                    p.write_contents()
+                p.write_contents()
+                if p.returncode != 0:  # Compilation failed
+                    self._write_to_stderr(
+                            "[CAF kernel] caf wrapper/GFortran exited with code {}, the executable will not be executed".format(
+                                    p.returncode))
+                    return {'status': 'ok', 'execution_count': self.execution_count, 'payload': [],
+                            'user_expressions': {}}
+
+        p = self.create_jupyter_subprocess(['cafrun', '-np', str(magics['num_procs']), binary_file.name] + magics['args'])
+        while p.poll() is None:
+            p.write_contents()
+        p.write_contents()
+
+        if p.returncode != 0:
+            self._write_to_stderr("[CAF kernel] Executable exited with code {}".format(p.returncode))
+        return {'status': 'ok', 'execution_count': self.execution_count, 'payload': [], 'user_expressions': {}}
+
+    def do_shutdown(self, restart):
+        """Cleanup the created source code files and executables when shutting down the kernel"""
+        self.cleanup_files()
---
+++ prebuild/jupyter-CAF-kernel/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(name='jupyter_CAF_kernel',
+      version='0.1.0',
+      description='Minimalistic Coarray Fortran kernel for Jupyter',
+      long_description='Minimalistic OpenCoarrays/GFortran based Coarray Fortran kernel for Jupyter',
+      author='Izaak Beekman',
+      author_email='contact@izaakbeekman.com',
+      url='https://github.com/sourceryinstitute/jupyter-CAF-kernel',
+      download_url='https://github.com/sourceryinstitute/jupyter-CAF-kernel',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Framework :: Jupyter',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Fortran',
+          'Topic :: Scientific/Engineering',
+      ],
+      packages=['jupyter_CAF_kernel'],
+      keywords=['jupyter', 'kernel', 'fortran', 'coarray', 'CAF', 'coarray-fortran']
+)
