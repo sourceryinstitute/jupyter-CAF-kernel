5f5d3811862d46a1485b805df3fc2cce65562abd
Izaak Beekman <zbeekman@gmail.com>
2017-11-10 11:29:34 -0500

    Improve magic handling

--- prebuild/jupyter-caf-kernel/jupyter_caf_kernel/kernel.py
+++ prebuild/jupyter-caf-kernel/jupyter_caf_kernel/kernel.py
@@ -6,6 +6,7 @@ import re
 import subprocess
 import tempfile
 import os
+from shlex import split as shsplit
 
 
 class RealTimeSubprocess(subprocess.Popen):
@@ -106,8 +107,10 @@ class CafKernel(Kernel):
                                   lambda contents: self._write_to_stderr(contents.decode()))
 
     def compile_with_caf_wrapper(self, source_filename, binary_filename, fcflags=None, ldflags=None):
-        _args = ['caf', source_filename] + fcflags + ['-o', binary_filename] + ldflags
-        return self.create_jupyter_subprocess(_args)
+        return self.create_jupyter_subprocess(['caf', source_filename] +
+                                              fcflags +
+                                              ['-o', binary_filename] +
+                                              ldflags)
 
     def _filter_magics(self, code):
 
@@ -117,20 +120,17 @@ class CafKernel(Kernel):
                   'num_images': os.getenv('NUM_IMAGES', '2').strip()
                 }
 
-
         for line in code.splitlines():
-            if line.startswith('%'):
-                key, value = [item.strip() for item in line[1:].split(":", 2)]
+            if line.strip().startswith('%'):
+                key, value = line.strip().strip('%').split(":", 2)
                 key = key.lower()
 
-                if key in ['ldflags', 'fcflags']:
-                    magics[key] = [flag.strip() for flag in value.split()]
-                elif key == 'args':
-                    # Split arguments respecting quotes
-                    for argument in re.findall(r'(?:[^\s,"]|"(?:\\.|[^"])*")+', value):
-                        magics['args'] += [argument.strip('"')]
+                if key in ['ldflags', 'fcflags', 'args']:
+                    magics[key] = shsplit(value)
                 elif key == 'num_images':
                     magics['num_images'] = int(value.strip())
+                else:
+                    pass # need to add exception handling
 
         return magics
 
@@ -156,9 +156,7 @@ class CafKernel(Kernel):
                                     p.returncode))
                     return {'status': 'ok', 'execution_count': self.execution_count, 'payload': [],
                             'user_expressions': {}}
-        _cmd = ['cafrun', '-np', str(magics['num_images']).strip(), binary_file.name ]
-        if magics['args']: _cmd + magics['args']
-        p = self.create_jupyter_subprocess(_cmd)
+        p = self.create_jupyter_subprocess(['cafrun', '-np', str(magics['num_images']).strip(), binary_file.name] + magics['args'])
         while p.poll() is None:
             p.write_contents()
         p.write_contents()
